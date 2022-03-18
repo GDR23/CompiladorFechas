b2f6fae697ca13ce53204fc36285680da1cbb01e
SupremeMaster23 <56442526+SupremeMaster23@users.noreply.github.com>
2022-03-18 15:03:15 -0600

    Add files via upload

---
+++ CompiladorFechas_Console/obj/anycpu/Debug/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Scanner;
+
+namespace CompiladorFechas_Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var contenido = File.ReadAllText("test.txt");
+            var logger = new Logger();
+            var scanner = new Scanner.Scanner(new Input(contenido), logger);
+            var parser = new Parser.Parser(scanner, logger);
+            parser.Parse();
+        }
+    }
+}
